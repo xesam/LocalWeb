e215af6a2ee8fb582d61989424abbf98234646a6
xesam <xesamguo@gmail.com>
2016-05-11 18:46:58 +0800

    cache

--- local-web/src/main/java/dev/xesam/android/localweb/LocalWebCache.java
+++ local-web/src/main/java/dev/xesam/android/localweb/LocalWebCache.java
@@ -3,17 +3,117 @@ package dev.xesam.android.localweb;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Created by xesamguo@gmail.com on 16-5-9.
  */
 public class LocalWebCache {
+
+    public static final int CONNECT_TIME_OUT = 30 * 1000;
+    public static final int READ_TIME_OUT = 2 * 60 * 1000;
+
+    private File getCacheDir(Context context) {
+        return context.getExternalCacheDir();
+    }
+
+    private File getDestFile(Context context, LocalWebRequest request) {
+        return new File(getCacheDir(context), request.getVersion() + ".zip");
+    }
+
     public void scan(Context context) {
-        File dir = context.getExternalCacheDir();
+        File dir = getCacheDir(context);
         String[] files = dir.list();
         for (String file : files) {
             Log.e("LocalWebCache", String.valueOf(file));
         }
+    }
+
+    public void sync(Context context, LocalWebRequest request) {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        try {
+            File dest = getDestFile(context, request);
+            URL url = new URL(request.getUrl());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(CONNECT_TIME_OUT);
+            conn.setReadTimeout(READ_TIME_OUT);
+            conn.connect();
+            bis = new BufferedInputStream(conn.getInputStream());
+            bos = new BufferedOutputStream(new FileOutputStream(dest));
+            byte[] buffer = new byte[8 * 1024];
+            int read;
+            while ((read = bis.read(buffer)) != -1) {
+                bos.write(buffer, 0, read);
+            }
+            bis.close();
+            bos.close();
+            process(context, request, new File(dest.getAbsolutePath()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void process(Context context, LocalWebRequest request, File zipFile) {
+        BufferedOutputStream bos = null;
+        try {
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+            ZipEntry entry = null;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.isDirectory()) {
+
+                } else {
+                    File target = new File(zipFile.getParent(), entry.getName());
+                    if (!target.getParentFile().exists()) {
+                        // 创建文件父目录
+                        target.getParentFile().mkdirs();
+                    }
+                    // 写入文件
+                    bos = new BufferedOutputStream(new FileOutputStream(target));
+                    int read;
+                    byte[] buffer2 = new byte[1024 * 10];
+                    while ((read = zis.read(buffer2, 0, buffer2.length)) != -1) {
+                        bos.write(buffer2, 0, read);
+                    }
+                    bos.flush();
+                }
+            }
+            zis.closeEntry();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
--- local-web/src/main/java/dev/xesam/android/localweb/LocalWebService.java
+++ local-web/src/main/java/dev/xesam/android/localweb/LocalWebService.java
@@ -6,25 +6,13 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
 /**
  * Created by xesamguo@gmail.com on 16-5-9.
  */
 public class LocalWebService extends IntentService {
 
     public static final String TAG = "LocalWebService";
+    private LocalWebCache mLocalWebCache;
 
     public LocalWebService() {
         super(TAG);
@@ -41,6 +29,7 @@ public class LocalWebService extends IntentService {
         if (LocalWebManager.DEBUG) {
             Log.d(TAG, "onCreate");
         }
+        mLocalWebCache = new LocalWebCache();
     }
 
     @Override
@@ -63,72 +52,11 @@ public class LocalWebService extends IntentService {
         }
 
         if (request == null) {
-            try {
-                Thread.sleep(1000);
-                onUpdated(request, new Bundle());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                InputStream in = null;
-                OutputStream out = null;
-                int connectTimeout = 30 * 1000; // 连接超时:30s
-                int readTimeout = 20 * 1000 * 1000; // IO超时:1min
-                byte[] buffer = new byte[8 * 1024]; // IO缓冲区:8KB
-
-                URL url = new URL(request.getUrl());
-                File file = new File(getExternalCacheDir(), request.getVersion() + ".zip");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-                conn.setConnectTimeout(connectTimeout);
-                conn.setReadTimeout(readTimeout);
-                conn.connect();
-
-                in = conn.getInputStream();
-                out = new FileOutputStream(file);
-
-                for (; ; ) {
-                    int bytes = in.read(buffer);
-                    if (bytes == -1) {
-                        break;
-                    }
-                    out.write(buffer, 0, bytes);
-
-                }
-                onUpdated(request, new Bundle());
-                in.close();
-                out.close();
-
-                ZipInputStream zis = new ZipInputStream(new FileInputStream(new File(getExternalCacheDir(), request.getVersion() + ".zip")));
-                BufferedOutputStream bos = null;
-                ZipEntry entry = null;
-                while ((entry = zis.getNextEntry()) != null) {
-                    if (entry.isDirectory()) {
-
-                    } else {
-                        File target = new File(file.getParent(), entry.getName());
-                        if (!target.getParentFile().exists()) {
-                            // 创建文件父目录
-                            target.getParentFile().mkdirs();
-                        }
-                        // 写入文件
-                        bos = new BufferedOutputStream(new FileOutputStream(target));
-                        int read;
-                        byte[] buffer2 = new byte[1024 * 10];
-                        while ((read = zis.read(buffer2, 0, buffer2.length)) != -1) {
-                            bos.write(buffer2, 0, read);
-                        }
-                        bos.flush();
-                    }
-                }
-                zis.closeEntry();
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            return;
         }
+
+        mLocalWebCache.sync(this, request);
+        onUpdated(request, new Bundle());
     }
 
     protected void onUpdated(LocalWebRequest request, Bundle responseReply) {
